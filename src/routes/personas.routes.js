91cd9a0d4a791376c9885993117e43d867242070
juanchodez <155789273+juanchodez@users.noreply.github.com>
2025-04-08 22:48:55 -0500

    Update personas.routes.js

--- src/routes/personas.routes.js
+++ src/routes/personas.routes.js
@@ -21,7 +21,7 @@ router.post('/add', async(req, res)=>{
     const newAsistente = {
             name, cover, regalo, asistencia
     }
-    pool.query('INSERT INTO personas SET (name, asistencia) VALUES ($1, $4)', [name, asistencia], (error, results); 
+    pool.query('INSERT INTO personas SET (name, asistencia) VALUES ($1, $4)', [name, asistencia], (error, results) response.status(200).json(responseReturn);) 
     pool.query('INSERT INTO asistentes SET (name, cover, regalo, asistencia) VALUES ($1, $2, $3, $4)', [name,cover, regalo, asistencia], (error, results) => {
         if (error) {
             throw error
